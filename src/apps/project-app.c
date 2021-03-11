4a97cd30b21eeb722ee4ee9a10aafa3538cd3380
kyang8093 <kgyang@stanford.edu>
2021-03-10 22:06:14 -0700

    updated scaling to screen size for initial velocity vector + max height/width calculations
    this is a working version for plotting trajectory + initial velocity.

--- src/apps/project-app.c
+++ src/apps/project-app.c
@@ -3,20 +3,34 @@
 #include "gl.h"
 #include "timer.h"
 #include "uart.h"
-
+/* Constants required for trig functions */
 #define CONST_PI  3.14159265
 #define CONST_2PI 6.28318531
+#define CONST_G 9.81
 #define modd(x, y) ((x) - (int)((x) / (y)) * (y))
 
-
-
+/* Constants for setting up screen width, height, etc. */
 const static int SCREEN_WIDTH = 800;
 const static int SCREEN_HEIGHT = 512;
 const static int GROUND_Y = SCREEN_HEIGHT - 50;
 
+/* 
+ * Scale factors set in angry_nerds_graphics_init at beginning of program. 
+ * These allow for scaling from virtual kinematics world to graphics world. 
+ * 
+ * x_scale_factor is defined as ratio between screen width and max width bird 
+ * can be launched to (from projectile motion).
+ * y_scale_factor is defined as ratio between screen height and max height bird
+ * can be launched to (from projectile motion).
+ */
+static double x_scale_factor;
+static double y_scale_factor;
+
 static struct bird_position_t {
     int x, y;
 } position;
+
+const double SCALE_FACTOR = 250.0; // factor to scale the force vector length by to get velocity (TODO: change later, depending on the force value we get)
 
 
 /*
@@ -46,6 +60,15 @@ static inline double sin(double x)
 }
 
 /*
+ * Use tanx = sinx/cosx.
+ */
+static inline double tan(double x) 
+{
+    return sin(x)/cos(x);
+}
+
+
+/*
  * Converts radians to degrees.
  */
 static inline double deg_to_rad(int degrees) 
@@ -65,29 +88,68 @@ static inline void gl_plot_ground(void) {
  * and 1 (100% force))
  * Plots the initial velocity vector from a point on the ground.
  */
-void gl_plot_initial_vector(double force, double angle) {
-    // initial position
-    gl_draw_rect(50, GROUND_Y, 10, 10, GL_AMBER);
+void gl_plot_initial_velocity_vector(double force, double angle) {
+    // plot initial position
+    gl_draw_rect(45, GROUND_Y - 5, 10, 10, GL_AMBER);
 
-    const double SCALE_FACTOR = 200.0; // factor to scale the force vector length by (TODO: change later, depending on the force value we get)
-
+    // virtual dx and dy, faithful to the actual angle supplied
     int dy = (int)((force * SCALE_FACTOR) * sin(angle));
     int dx = (int)((force * SCALE_FACTOR) * cos(angle));
 
-    gl_draw_line(50, GROUND_Y, 50 + dx, GROUND_Y - dy, GL_BLACK);
+    int dy_scaled = dy * y_scale_factor;
+    int dx_scaled = dx * x_scale_factor;
+
+    // plot line for initial velocity vector
+    gl_draw_line(50, GROUND_Y, 50 + dx_scaled, GROUND_Y - dy_scaled, GL_BLACK);
 
 }
 
 
-void gl_plot_trajectory(int angle, int force) {
-    // initial position
+int calc_max_height(double force, double angle) {
+    double velocity = force * SCALE_FACTOR;
+    return (int) (velocity * velocity / (4 * CONST_G));
+}
+
+
+void gl_plot_trajectory(double force, double angle) {
+    // set position to initial position
     position.x = 50;
     position.y = GROUND_Y;
+
+    double velocity = force * SCALE_FACTOR;
+
+    // use kinematics trajectory equation:
+    //y = h + xtantheta - g/(2v^2cos^2theta)x^2
+    while(position.x < SCREEN_WIDTH && position.y >= 0) {
+        // virtual x and virtual y represent the actual values of x and y generated from the kinematics trajectory equation
+        int virtual_x = (position.x - 50) / x_scale_factor;
+        int virtual_y = (int) ((virtual_x * tan(angle) - CONST_G/(2 * velocity *velocity * cos(angle) * cos(angle)) * virtual_x * virtual_x)); // determine virtual y-position
+
+        // actual x position is already stored in position.x
+        // convert virtual y to actual y position and store in position.y
+        position.y = GROUND_Y - virtual_y*y_scale_factor;
+
+        gl_draw_pixel(position.x, position.y, GL_AMBER);
+        // printf("virtual x: %d, virtual y: %d, actual x: %d, actual y: %d\n", virtual_x, virtual_y, position.x, position.y);
+
+        position.x++; // increment to next x-value
+    }
+}
+
+void angry_nerds_graphics_init(void)
+{
+    // calculate max height and width the bird can be launched to from kinematic principles.
+    // maximum height results from maximum force (1.0) applied at 45 degree angle.
+    int max_height = calc_max_height(1.0, deg_to_rad(45));
+    int max_width = 4 * max_height;
+
+    // set scale factor
+    y_scale_factor = (double) GROUND_Y / max_height;
+    x_scale_factor = (double) (SCREEN_WIDTH - 50) / max_width;
 }
 
 void main(void)
 {
-
     uart_init();
     gl_init(SCREEN_WIDTH, SCREEN_HEIGHT, GL_DOUBLEBUFFER);
 
@@ -95,15 +157,21 @@ void main(void)
     gl_clear(gl_color(0x55, 0, 0x55));
     timer_delay(3);
 
+    angry_nerds_graphics_init();
+
     // plot ground
     gl_plot_ground();
 
-    // plot initial vector from starting point given angle and force
-    gl_plot_initial_vector(2.0, deg_to_rad(30));
+
+    // plot trajectory of bird given angle and force
+    gl_plot_trajectory(1.0, deg_to_rad(45));
+
+    // plot initial velocity vector given angle and force
+    gl_plot_initial_velocity_vector(1.0, deg_to_rad(45));
+
 
     // Show buffer with drawn contents
     gl_swap_buffer();
 
-    printf("Hello, world!\n");
     uart_putchar(EOT);
 }

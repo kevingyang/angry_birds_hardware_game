c1e2e9e6c78b19ad022453baf87a75e3443b64ad
kyang8093 <kgyang@stanford.edu>
2021-03-16 14:00:21 -0600

    basic game difficulty setup

--- src/apps/project-app.h
+++ src/apps/project-app.h
@@ -5,9 +5,10 @@ struct position_t {
     int x, y;
 };
 
-/* position_t position struct stores the coordinates of bird's current position. */
+/* position_t position struct stores the coordinates of bird's current position, and target_position stores the coordinates of the target's position. */
 struct position_t bird_position;
 struct position_t target_position;
+unsigned int TARGET_SIZE; // stores the size of the target (the width/height of the square)
 
 /* Constants for setting up screen width, height, etc. */
 int SCREEN_WIDTH; // set the screen width and height to match your screen size!
@@ -28,13 +29,21 @@ double y_scale_factor;
 
 double velocity_scale_factor; // factor to scale the force vector length by to get velocity (TODO: change later, depending on the force value we get)
 
+/* Keyboard initialization setup */
+#define KEYBOARD_CLOCK GPIO_PIN3
+#define KEYBOARD_DATA GPIO_PIN4
+
+
 void gl_plot_ground(int ground_y_value);
 void gl_plot_initial_velocity_vector(double force, double angle, color_t color);
 void gl_plot_trajectory(double force, double angle, color_t color);
 int calc_max_height(double force, double angle);
 void gl_draw_image(unsigned int x, unsigned int y, char first_initial);
 void gl_plot_image_trajectory(double force, double angle, char first_initial);
-void angry_nerds_graphics_init(void);
 void gl_draw_target(unsigned int leftBound, unsigned int size);
+
+void angry_nerds_graphics_init(void);
+void angry_nerds_game_init(void);
+void angry_nerds_game_start(unsigned int difficulty);
 
 #endif

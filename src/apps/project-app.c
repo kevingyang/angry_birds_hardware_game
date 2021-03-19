93cc381ea719c207c00f9596aa60e2d5e01e56a8
selenasun1618 <selenas@stanford.edu>
2021-03-19 05:39:38 -0700

    revised proposal.md

--- src/apps/project-app.c
+++ src/apps/project-app.c
@@ -456,8 +456,7 @@ void angry_nerds_game_start(unsigned int difficulty) {
 void main (void)
 {
     uart_init();
-    printf("after uart_init()\n");
     angry_nerds_game_init(); // start the angry nerds game!
-    printf("after game_init()"\n);
+    printf("after game_init()\n");
     uart_putchar(EOT);
 }
